78e8baf970d690cf5685a11512a002a6fb61d908
sreeparna <sreeparnabsraju2k1@gmail.com>
2022-12-12 12:37:28 +0530

    updated service

--- src/app/admin/http-client.service.ts
+++ src/app/admin/http-client.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Faculty, User } from 'src/app/models/user';
+import { Faculty } from 'src/app/models/user';
 
 export class Branch{
   constructor(
@@ -8,6 +8,15 @@ export class Branch{
   ){}
 }
 
+export class Course{
+  constructor(
+    public courseName:string
+  ){}
+}
+
+const baseUrl='http://localhost:3030/api/';
+/* const baseUrl1='http://localhost:7878/api/add-branch';
+const baseUrl2='http://localhost:7878/api/add-course'; */
 @Injectable({
   providedIn: 'root'
 })
@@ -15,12 +24,43 @@ export class HttpClientService {
 
   constructor(private httpClient:HttpClient) { }
 
-  public createFaculty(faculty: Faculty){
-    return this.httpClient.post<Faculty>("http://localhost:3030/addStaff", faculty);
+  getFaculty(){
+    console.log("test call");
+    return this.httpClient.get<Faculty[]>(baseUrl)
   }
+
+  public deleteFaculty(faculty: Faculty) {
+    return this.httpClient.delete<Faculty>(baseUrl + "/"+ faculty.firstName);
+  }
+
+  public createFaculty(faculty: Faculty){
+    return this.httpClient.post<Faculty>(baseUrl, faculty);
+  }
+
+  getBranch(){
+    console.log("test call");
+    return this.httpClient.get<Branch[]>('http://localhost:3030/Brancs')
+  }
+
+  public deleteBranch(branch: Branch) {
+    return this.httpClient.delete<Branch>("http://localhost:3030" + "/"+ branch.branchName);
+  } 
 
   public createBranches(branch:Branch){
     return this.httpClient.post<Branch>("http://localhost:3030/addBranch", branch);
+  }
+
+  getCourse(){
+    console.log("test call");
+    return this.httpClient.get<Course[]>('http://localhost:3030/Cours')
+  }
+
+  public deleteCourse(course: Course) {
+    return this.httpClient.delete<Course>("http://localhost:3030" + "/"+ course.courseName);
+  } 
+
+  public createCourses(course:Course){
+    return this.httpClient.post<Course>("http://localhost:3030/addCourse", course);
   }
 
 }
